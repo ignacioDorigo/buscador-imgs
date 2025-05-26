53659f9dfef8d9e3137169e7665b776e051f7d4d
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2025-05-26 19:37:37 -0300

    Formulario basico

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginForm.css";
 
 function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const imprimirFormulario = (e) => {
+    console.log("email: " + email);
+    console.log("password: " + password);
+    e.preventDefault();
+  };
+
   return (
     <form className="loginForm">
       <div className="div__campo__formulario">
-        <label>Email</label>
-        <input type="email"></input>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        ></input>
       </div>
 
       <div className="div__campo__formulario">
-        <label>Password</label>
-        <input type="password"></input>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        ></input>
       </div>
-      <button className="btn_formulario">Ingresar</button>
+      <button className="btn_formulario" onClick={(e) => imprimirFormulario(e)}>
+        Ingresar
+      </button>
     </form>
   );
 }
